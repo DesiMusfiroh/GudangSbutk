fc784767c5bc62362e43166212022667a4f40bb0
DesiMusfiroh <musfiroh.desi@gmail.com>
2021-08-28 22:39:19 +0700

    chip navigation bottom

--- app/src/main/java/com/ptpn/gudangsbutk/ui/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/ptpn/gudangsbutk/ui/dashboard/DashboardActivity.kt
@@ -2,11 +2,44 @@ package com.ptpn.gudangsbutk.ui.dashboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.ptpn.gudangsbutk.R
+import com.ptpn.gudangsbutk.databinding.ActivityDashboardBinding
+import com.ptpn.gudangsbutk.ui.data.DataFragment
+import com.ptpn.gudangsbutk.ui.form.FormFragment
+import com.ptpn.gudangsbutk.ui.home.HomeFragment
+import com.ptpn.gudangsbutk.ui.settings.SettingsFragment
 
 class DashboardActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDashboardBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.navView.setItemSelected(R.id.navigation_home, true)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.nav_host_fragment, HomeFragment())
+            .commit()
+
+        bottomMenu()
+    }
+
+    private fun bottomMenu() {
+        binding.navView.setOnItemSelectedListener {
+            val fragment: Fragment = when (it) {
+                R.id.navigation_form ->  FormFragment()
+                R.id.navigation_data ->  DataFragment()
+                R.id.navigation_settings -> SettingsFragment()
+                else -> HomeFragment()
+            }
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.nav_host_fragment, fragment)
+                .commit()
+        }
     }
 }
